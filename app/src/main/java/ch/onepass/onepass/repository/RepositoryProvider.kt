5cbe651380007eb81e0c67570c6ba235eb2bff7c
rayd418 <rayane.drissi@epfl.ch>
2025-11-21 03:46:44 +0100

    fix: format

--- app/src/main/java/ch/onepass/onepass/repository/RepositoryProvider.kt
+++ app/src/main/java/ch/onepass/onepass/repository/RepositoryProvider.kt
@@ -2,10 +2,10 @@ package ch.onepass.onepass.repository
 
 import ch.onepass.onepass.model.event.EventRepository
 import ch.onepass.onepass.model.event.EventRepositoryFirebase
-import ch.onepass.onepass.model.storage.StorageRepository
-import ch.onepass.onepass.model.storage.StorageRepositoryFirebase
 import ch.onepass.onepass.model.map.LocationRepository
 import ch.onepass.onepass.model.map.NominatimLocationRepository
+import ch.onepass.onepass.model.storage.StorageRepository
+import ch.onepass.onepass.model.storage.StorageRepositoryFirebase
 
 object RepositoryProvider {
   private val _eventRepository: EventRepository by lazy { EventRepositoryFirebase() }
