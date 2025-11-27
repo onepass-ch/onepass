70c534d018861201c16f914ebec94310a735e214
Hassan372 <baddourhassan@gmail.com>
2025-11-28 00:12:38 +0100

    refactor: move CameraX dependencies to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -165,10 +165,11 @@ dependencies {
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
 
     // CameraX
-    implementation("androidx.camera:camera-core:1.3.4")
-    implementation("androidx.camera:camera-camera2:1.3.4")
-    implementation("androidx.camera:camera-lifecycle:1.3.4")
-    implementation("androidx.camera:camera-view:1.3.4")
+
+    implementation(libs.camerax.core)
+    implementation(libs.camerax.camera2)
+    implementation(libs.camerax.lifecycle)
+    implementation(libs.camerax.view)
 
 
     // ------------- Firebase ------------------
