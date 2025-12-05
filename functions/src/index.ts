db6f73790ee6d4344aef40bdbaacb0f694c3aff8
Hassan372 <baddourhassan@gmail.com>
2025-12-05 11:58:53 +0100

    seperate index.ts into exports and searchUsers.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
-import * as admin from "firebase-admin";
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at __https://firebase.google.com/docs/functions__
+ */
 
+import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export { validateEntryByPass } from "./validateEntryByPass";
 export { generateUserPass } from "./generateUserPass";
 export { onUserCreated } from "./onUserCreated";
 export { createPaymentIntent, stripeWebhook } from "./stripe";
-//export { searchUsers } from "./searchUsers";
+export { searchUsers } from "./searchUsers";
