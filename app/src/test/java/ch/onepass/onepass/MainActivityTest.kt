ce146028a100e18ec408f4a7929cccc688d213d0
E. <203526535+M-e-d-A@users.noreply.github.com>
2025-12-15 16:27:44 +0100

    test(activity): fix MainActivityTest by initializing FirebaseApp
    Added FirebaseApp initialization in MainActivityTest setup to prevent
    NullPointerException when accessing Firebase Functions. Ensures tests run
    correctly in isolated test environment.

--- app/src/test/java/ch/onepass/onepass/MainActivityTest.kt
+++ app/src/test/java/ch/onepass/onepass/MainActivityTest.kt
@@ -1,12 +1,22 @@
 package ch.onepass.onepass
 
+import com.google.firebase.FirebaseApp
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
+
+  @Before
+  fun setup() {
+    if (FirebaseApp.getApps(RuntimeEnvironment.getApplication()).isEmpty()) {
+      FirebaseApp.initializeApp(RuntimeEnvironment.getApplication())
+    }
+  }
 
   @Test
   fun onCreate_executesStripeInitializationCode() {
